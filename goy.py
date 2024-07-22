f6c7af255c284e86e84f733ee818f9c7cbbeb782
Piyush-test <piyush2005thebest@gmail.com>
2024-07-22 10:10:47 +0530

    Piyush's changes

---
+++ goy.py
@@ -0,0 +1,39 @@
+#Function to add two numbers
+def add(x, y): return x + y
+
+#Function to subtract two numbers
+def subtract(x, y): return x - y
+
+#Function to multiply two numbers
+def multiply(x, y): return x * y
+
+#Function to divide two numbers
+def divide(x, y): 
+    if y == 0: 
+        return "Error! Division by zero." 
+    else: 
+        return x / y
+
+#Main function to operate the calculator
+def calculator(): print("Select operation:") 
+print("1. Addition") 
+print("2. Subtraction") 
+print("3. Multiplication") 
+print("4. Division")
+
+# Take input from the user
+choice = input("Enter choice (1/2/3/4): ")
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+if choice == '1':
+    print(num1, "+", num2, "=", add(num1, num2))
+elif choice == '2':
+    print(num1, "-", num2, "=", subtract(num1, num2))
+elif choice == '3':
+    print(num1, "*", num2, "=", multiply(num1, num2))
+elif choice == '4':
+    print(num1, "/", num2, "=", divide(num1, num2))
+else:
+    print("Invalid input")
